ba75442d8918e6de56820c412b5dc356bded6cf8
Filip <93220072+toLLto@users.noreply.github.com>
2025-04-01 14:35:13 +0200

    feat: Added colors + RasTerX update

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ using namespace std;
 const unsigned int WIDTH = 256;
 const unsigned int HEIGHT = 256;
 
+const unsigned int BLACK = 0xFF000000;
+const unsigned int WHITE = 0xFFFFFFFF;
+const unsigned int RED = 0xFFFF0000;
+const unsigned int GREEN = 0xFF00FF00;
+const unsigned int BLUE = 0xFF0000FF;
+const unsigned int YELLOW = 0xFFFFFF00;
+const unsigned int MAGENTA = 0xFFFF00FF;
+const unsigned int CYAN = 0xFF00FFFF;
+
 int main() 
 {
     Rasterizer::Buffer buffer(WIDTH, HEIGHT);
@@ -14,12 +23,12 @@ int main()
     Rasterizer::Rasterizer rasterizer(WIDTH, HEIGHT);
 
     Triangle triangle(
-        Vector3(0.f, -0.4f, 0.f),
-        Vector3(-0.8f, 0.2f, 0.f),
-        Vector3(0.3f, 0.9f, 0.f)
+        Vector3(0.f, 0.5f, 0.f),
+        Vector3(0.6f, -0.5f, 0.f),
+        Vector3(-0.6f, -0.5f, 0.f)
     );
 
-    rasterizer.Render(triangle, 0xFFFF0000, 0xFF000000);
+    rasterizer.Render(triangle, RED, BLACK);
 
     return 0;
 }
